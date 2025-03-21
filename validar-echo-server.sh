cb62549036884a23a97b289bdefa22384364da44
Tomás <tomivenitez.14@gmail.com>
2025-03-21 17:39:01 -0300

    Fix: fix comparison bash.

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -5,7 +5,7 @@ MESSAGE="Hello"
 
 RESPONSE=$(docker run --rm --network testing_net busybox:latest sh -c "echo '$MESSAGE' | nc $SERVER")
 
-if [[ "$RESPONSE" == "$MESSAGE" ]]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
